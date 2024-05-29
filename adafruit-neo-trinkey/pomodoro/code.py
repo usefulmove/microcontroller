d1090a3ad5a92002344d637f3d58f053332cab8f
Duane Edmonds <duane.edmonds@gmail.com>
2024-05-28 19:06:26 -0700

    completed initial development and testing

--- adafruit-neo-trinkey/pomodoro/code.py
+++ adafruit-neo-trinkey/pomodoro/code.py
@@ -1,28 +1,61 @@
-import time
 import board
 import neopixel
+import time
 import touchio
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, brightness=0.05)
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, brightness=0.03)
 
+# capacitive touch sensors
 touch1 = touchio.TouchIn(board.TOUCH1)
 touch2 = touchio.TouchIn(board.TOUCH2)
 
-while True:
+# pomodoro durations
+durations = [10, 20, 30, 60]
+duration = durations[1]
+
+# neopixel colors
+active = (0, 0, 255)
+done = (0, 255, 0)
+mode = (155, 100, 0)
+
+def pixels_off():
     pixels.fill((0, 0, 0))
+
+def pixels_on(color=(85, 85, 85), number=4):
+    for i in range(number):
+        pixels[i] = color
+
+while True:
+    pixels_off()
+
     if (touch1.value == True):
         start_time = time.monotonic()
         step = 0
-        while (time.monotonic() - start_time) <= 20 * 60:
-            pixels[step % 4] = (0, 25, 230)
+        while (time.monotonic() - start_time) < duration * 60:
+            pixels[step % 4] = active
             time.sleep(0.120)
-            pixels.fill((0, 0, 0))
+            pixels_off()
             step = step + 1
         time.sleep(0.25)
-        pixels.fill((0, 255, 0))
+        pixels_on(done)
         time.sleep(0.1)
-        pixels.fill((0, 0, 0))
+        pixels_off()
         time.sleep(0.1)
-        pixels.fill((0, 255, 0))
+        pixels_on(done)
         time.sleep(2.0)
-        pixels.fill((0, 0, 0))
+        pixels_off()
+
+    if (touch2.value == True):
+        if (duration == durations[0]):
+            duration = durations[1]
+            pixels_on(mode, 2)
+        elif (duration == durations[1]):
+            duration = durations[2]
+            pixels_on(mode, 3)
+        elif (duration == durations[2]):
+            duration = durations[3]
+            pixels_on(mode)
+        else:
+            duration = durations[0]
+            pixels_on(mode, 1)
+        time.sleep(1.0)
