526361daad70b6b558c00041daecde09d72f4a79
Duane Edmonds <duane.edmonds@gmail.com>
2024-06-28 13:01:07 -0700

    added version. completed code review enhancements

--- adafruit-esp32-s3-tft-feather/circuitpython/popcorn_reader/code.py
+++ adafruit-esp32-s3-tft-feather/circuitpython/popcorn_reader/code.py
@@ -6,12 +6,26 @@ import time
 from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text import label
 
+splash = "popcorn"
+version = "0.0.2"
+
 # reading parameters
 words_per_minute = 280
 sentence_pause = 1.2
 comma_pause = 0.4
 
 word_pause = 60 / words_per_minute
+
+# font colors
+splash_font_color = 0xf9e4bc
+reader_font_color = 0xfaf6ea
+author_font_color = 0xef9d6e
+version_font_color = 0x202020
+
+# font paths
+splash_font_path = "/fonts/Comforta-Regular-20.bdf"
+reader_font_path = "/fonts/SourceCodePro-Regular-18.bdf"
+author_font_path = "/fonts/Comforta-Regular-20.bdf"
 
 # message to display
 message = "\"The capacity to be alone is the capacity to love. It may look paradoxical to you, but it's not. It is an existential truth: only those people who are capable of being alone are capable of love, of sharing, of going into the deepest core of another person without possessing the other, without becoming dependent on the other, without reducing the other to a thing, and without becoming addicted to the other. They allow the other absolute freedom, because they know that if the other leaves, they will be as happy as they are now. Their happiness cannot be taken by the other, because it is not given by the other.\""
@@ -23,21 +37,22 @@ author = "Osho"
 #message = "\"You think your pain and your heartbreak are un- precedented in the history of the world, but then you read. It was books that taught me that the things that tormented me most were the very things that connected me with all the people who were alive, who had ever been alive.\""
 #author = "J. Baldwin"
 
-splash = "popcorn"
+words = message.split()
 
 # set up display
 display = displayio.Group()
 board.DISPLAY.root_group = display
 
 # load fonts
-splash_font = bitmap_font.load_font("/fonts/Comforta-Regular-20.bdf")
-reader_font = bitmap_font.load_font("/fonts/SourceCodePro-Regular-18.bdf")
-author_font = bitmap_font.load_font("/fonts/Comforta-Regular-20.bdf")
+splash_font = bitmap_font.load_font(splash_font_path)
+reader_font = bitmap_font.load_font(reader_font_path)
+author_font = bitmap_font.load_font(author_font_path)
 
 # configure labels
-splash_label = label.Label(splash_font, text="", color=0xffffff, x=24, y=60)
-reader_label = label.Label(reader_font, text="", color=0xf9e4bc, x=24, y=58)
-author_label = label.Label(author_font, text="", color=0xef9d6e, x=24, y=58)
+splash_label = label.Label(splash_font, text="", color=splash_font_color, x=24, y=60)
+reader_label = label.Label(reader_font, text="", color=reader_font_color, x=24, y=58)
+author_label = label.Label(author_font, text="", color=author_font_color, x=24, y=58)
+version_label = label.Label(terminalio.FONT, text=version, color=version_font_color, x=200, y=120)
 
 cache_splash_label = label.Label(splash_font, text="", color=0x000000, x=24, y=60)
 cache_reader_label = label.Label(reader_font, text="", color=0x000000, x=24, y=58)
@@ -46,6 +61,7 @@ cache_author_label = label.Label(author_font, text="", color=0x000000, x=24, y=5
 display.append(splash_label)
 display.append(reader_label)
 display.append(author_label)
+display.append(version_label)
 
 # configure boot button
 boot_button = digitalio.DigitalInOut(board.BOOT0)
@@ -58,32 +74,36 @@ def clear_display(delay=0):
     author_label.text = ""
     time.sleep(delay)
 
-def display_typed_text(label, text, delay=0.05):
+# display with typewriter effect
+def display_typed_text(label, text, delay=0.05, cache_label=None):
+    if cache_label is not None:
+        for i in range(len(text) + 1):
+            cache_label.text = text[:i] + "|"
+
     for i in range(len(text) + 1):
         label.text = text[:i] + "|"
         time.sleep(delay)
     label.text = text
 
 while True:
-    display_typed_text(cache_splash_label, splash, 0)
-    display_typed_text(splash_label, splash)
+    # display splash
+    display_typed_text(splash_label, splash, cache_label=cache_splash_label)
 
     # wait for button press
     while boot_button.value:
         pass
 
-    # clear display
     clear_display()
 
     # display words
-    for word in message.split(): # cache word bitmaps for speed of rendering
+    for word in words: # cache bitmaps
         cache_reader_label.text = word.lower()
     clear_display()
 
-    for word in message.split():
+    for word in words:
         reader_label.text = word.lower()
 
-        if word[-1] in {".", "?", "!", "\"", ";", "—", "–", "-"}:
+        if word[-1] in {".", "?", "!", "\"", ";", "—", "–"}:
             time.sleep(sentence_pause)
         elif word[-1] in {",", ":"}:
             time.sleep(comma_pause)
@@ -91,13 +111,10 @@ while True:
         time.sleep(word_pause)
     time.sleep(0.8)
 
-    # clear display
     clear_display(1)
 
-    # display author with typewriter effect
-    display_typed_text(cache_author_label, author, 0)
-    display_typed_text(author_label, author)
-    time.sleep(4)
+    # display author
+    display_typed_text(author_label, author, cache_label=cache_author_label)
+    time.sleep(3.5)
 
-    # clear display
     clear_display(1)
