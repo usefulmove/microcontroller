5ad09159c602fa16c8bc8c7815e171d73e74b32d
Duane Edmonds <duane.edmonds@gmail.com>
2024-06-28 10:02:41 -0700

    added caching to speed up word rendering

--- adafruit-esp32-s3-tft-feather/circuitpython/reader/code.py
+++ adafruit-esp32-s3-tft-feather/circuitpython/reader/code.py
@@ -7,44 +7,45 @@ from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text import label
 
 # reading parameters
-words_per_minute = 240
+words_per_minute = 280
 sentence_pause = 1.2
 comma_pause = 0.4
 
 word_pause = 60 / words_per_minute
 
 # message to display
-message = "The capacity to be alone is the capacity to love. It may look paradoxical to you, but it's not. It is an existential truth: only those people who are capable of being alone are capable of love, of sharing, of going into the deepest core of another person without possessing the other, without becoming dependent on the other, without reducing the other to a thing, and without becoming addicted to the other. They allow the other absolute freedom, because they know that if the other leaves, they will be as happy as they are now. Their happiness cannot be taken by the other, because it is not given by the other."
+message = "\"The capacity to be alone is the capacity to love. It may look paradoxical to you, but it's not. It is an existential truth: only those people who are capable of being alone are capable of love, of sharing, of going into the deepest core of another person without possessing the other, without becoming dependent on the other, without reducing the other to a thing, and without becoming addicted to the other. They allow the other absolute freedom, because they know that if the other leaves, they will be as happy as they are now. Their happiness cannot be taken by the other, because it is not given by the other.\""
 author = "Osho"
 
-#message = "Physical concepts are free creations of the human mind, and are not, however it may seem, uniquely determined by the external world. In our endeavor to understand reality we are somewhat like a man trying to understand the mechanism of a closed watch. He sees the face and the moving hands, even hears its ticking, but he has no way of opening the case. If he is ingenious, he may form some picture of a mechanism which could be responsible for all the things he observes, but he may never be quite sure his picture is the only one which could explain his observations. He will never be able to compare his picture with the real mechanism and he cannot even imagine the possibility or the meaning of such a comparison."
+#message = "\"Physical concepts are free creations of the human mind, and are not, however it may seem, uniquely determined by the external world. In our endeavor to understand reality we are somewhat like a man trying to understand the mechanism of a closed watch. He sees the face and the moving hands, even hears its ticking, but he has no way of opening the case. If he is ingenious, he may form some picture of a mechanism which could be responsible for all the things he observes, but he may never be quite sure his picture is the only one which could explain his observations. He will never be able to compare his picture with the real mechanism and he cannot even imagine the possibility or the meaning of such a comparison.\""
 #author = "A. Einstein"
 
-#message = "You think your pain and your heartbreak are un- precedented in the history of the world, but then you read. It was books that taught me that the things that tormented me most were the very things that connected me with all the people who were alive, who had ever been alive."
+#message = "\"You think your pain and your heartbreak are un- precedented in the history of the world, but then you read. It was books that taught me that the things that tormented me most were the very things that connected me with all the people who were alive, who had ever been alive.\""
 #author = "J. Baldwin"
 
-prompt = "( press boot button to begin )"
+prompt = ". . ."
 
 # set up display
 display = displayio.Group()
 board.DISPLAY.root_group = display
 
 # load fonts
-font = bitmap_font.load_font("/fonts/SourceCodePro-Regular-18.bdf")
-author_font = bitmap_font.load_font("/fonts/SourceCodePro-Regular-20.bdf")
+splash_font = bitmap_font.load_font("/fonts/Comforta-Regular-16.bdf")
+reader_font = bitmap_font.load_font("/fonts/SourceCodePro-Regular-18.bdf")
+author_font = bitmap_font.load_font("/fonts/Comforta-Regular-20.bdf")
 
 # configure labels
-message_label = label.Label(terminalio.FONT, text=prompt, color=0x606060, x=24, y=60)
-display.append(message_label)
+splash_label = label.Label(splash_font, text="", color=0x202020, x=24, y=60)
+reader_label = label.Label(reader_font, text="", color=0xf9e4bc, x=24, y=58)
+dummy_label = label.Label(reader_font, text="", color=0x000000, x=24, y=58)
+author_label = label.Label(author_font, text="", color=0xef9d6e, x=24, y=58)
 
-reader_label = label.Label(font, text="", color=0xf9e4bc, x=24, y=60)
+display.append(splash_label)
 display.append(reader_label)
-
-author_label = label.Label(author_font, text="", color=0xef9d6e, x=24, y=60)
 display.append(author_label)
 
 def clear_display(delay=0):
-    message_label.text = ""
+    splash_label.text = ""
     reader_label.text = ""
     author_label.text = ""
     time.sleep(delay)
@@ -54,28 +55,26 @@ boot_button = digitalio.DigitalInOut(board.BOOT0)
 boot_button.direction = digitalio.Direction.INPUT
 boot_button.pull = digitalio.Pull.UP
 
+# cache word bitmaps for speed of rendering
+for word in message.split():
+    dummy_label.text = word.lower()
+clear_display()
+
 while True:
-    message_label.text = prompt
+    splash_label.text = prompt
 
     # wait for button press
     while boot_button.value:
         pass
 
     # clear display
-    clear_display(1)
-
-    # display author
-    author_label.text = author
-    time.sleep(2)
-
-    # clear display
     clear_display(2)
 
     # display words
     for word in message.split():
-        reader_label.text = word
+        reader_label.text = word.lower()
 
-        if word[-1] in {".", "?", "!", "\"", ";", "—"}:
+        if word[-1] in {".", "?", "!", "\"", ";", "—", "–", "-"}:
             time.sleep(sentence_pause)
         elif word[-1] in {",", ":"}:
             time.sleep(comma_pause)
@@ -84,4 +83,11 @@ while True:
     time.sleep(1)
 
     # clear display
-    clear_display(3)
+    clear_display(2)
+
+    # display author
+    author_label.text = author
+    time.sleep(3)
+
+    # clear display
+    clear_display()
