3ecd7c5223f14e156056d43d00d8dbb31fb52bb2
Duane Edmonds <duane.edmonds@gmail.com>
2024-06-28 17:08:57 -0700

    implemented rotating messages

--- adafruit-esp32-s3-tft-feather/circuitpython/popcorn_reader/code.py
+++ adafruit-esp32-s3-tft-feather/circuitpython/popcorn_reader/code.py
@@ -10,7 +10,7 @@ splash = "popcorn"
 version = "0.0.3"
 
 # reading parameters
-words_per_minute = 280
+words_per_minute = 260
 sentence_pause = 1.2
 comma_pause = 0.4
 
@@ -24,7 +24,7 @@ screen_width = 240
 splash_font_color = 0xf9e4bc
 reader_font_color = 0xfaf6ea
 author_font_color = 0xef9d6e
-version_font_color = 0x202020
+version_font_color = 0x282828
 
 # font paths
 splash_font_path = "/fonts/Comforta-Regular-20.bdf"
@@ -32,16 +32,17 @@ reader_font_path = "/fonts/SourceCodePro-Regular-18.bdf"
 author_font_path = "/fonts/Comforta-Regular-20.bdf"
 
 # message to display
-message = "\"The capacity to be alone is the capacity to love. It may look paradoxical to you, but it's not. It is an existential truth: only those people who are capable of being alone are capable of love, of sharing, of going into the deepest core of another person without possessing the other, without becoming dependent on the other, without reducing the other to a thing, and without becoming addicted to the other. They allow the other absolute freedom, because they know that if the other leaves, they will be as happy as they are now. Their happiness cannot be taken by the other, because it is not given by the other.\""
-author = "Osho"
+messages = []
+authors = []
 
-#message = "\"Physical concepts are free creations of the human mind, and are not, however it may seem, uniquely determined by the external world. In our endeavor to understand reality we are somewhat like a man trying to understand the mechanism of a closed watch. He sees the face and the moving hands, even hears its ticking, but he has no way of opening the case. If he is ingenious, he may form some picture of a mechanism which could be responsible for all the things he observes, but he may never be quite sure his picture is the only one which could explain his observations. He will never be able to compare his picture with the real mechanism and he cannot even imagine the possibility or the meaning of such a comparison.\""
-#author = "A. Einstein"
+messages.append("\"The capacity to be alone is the capacity to love. It may look paradoxical to you, but it's not. It is an existential truth: only those people who are capable of being alone are capable of love, of sharing, of going into the deepest core of another person without possessing the other, without becoming dependent on the other, without reducing the other to a thing, and without becoming addicted to the other. They allow the other absolute freedom, because they know that if the other leaves, they will be as happy as they are now. Their happiness cannot be taken by the other, because it is not given by the other.\"")
+authors.append("Osho")
 
-#message = "\"You think your pain and your heartbreak are un- precedented in the history of the world, but then you read. It was books that taught me that the things that tormented me most were the very things that connected me with all the people who were alive, who had ever been alive.\""
-#author = "J. Baldwin"
+messages.append("\"Physical concepts are free creations of the human mind, and are not, however it may seem, uniquely determined by the external world. In our endeavor to understand reality we are somewhat like a man trying to understand the mechanism of a closed watch. He sees the face and the moving hands, even hears its ticking, but he has no way of opening the case. If he is ingenious, he may form some picture of a mechanism which could be responsible for all the things he observes, but he may never be quite sure his picture is the only one which could explain his observations. He will never be able to compare his picture with the real mechanism and he cannot even imagine the possibility or the meaning of such a comparison.\"")
+authors.append("A. Einstein")
 
-words = message.split()
+messages.append("\"You think your pain and your heartbreak are un- precedented in the history of the world, but then you read. It was books that taught me that the things that tormented me most were the very things that connected me with all the people who were alive, who had ever been alive.\"")
+authors.append("J. Baldwin")
 
 # set up display
 display = displayio.Group()
@@ -94,7 +95,7 @@ cache_reader_label = label.Label(
     text="",
     color=0x000000,
     anchor_point=(0.5, 0.5),
-    anchored_position=(screen_width//2, screen_height//2)
+    anchored_position=(screen_width//2, screen_height//2),
 )
 cache_author_label = label.Label(
     author_font,
@@ -131,17 +132,24 @@ def display_typed_text(label, text, delay=0.05, cache_label=None):
         time.sleep(delay)
     label.text = text
 
+message_index = 0
 while True:
     # display splash
-    display_typed_text(splash_label, splash, cache_label=cache_splash_label)
+    display_typed_text(
+        splash_label,
+        splash,
+        cache_label=cache_splash_label
+    )
 
     # wait for button press
     while boot_button.value:
         pass
 
-    clear_display()
-
+    clear_display(2)
+    
     # display words
+    words = messages[message_index].split()
+
     for word in words: # cache bitmaps
         cache_reader_label.text = word.lower()
     clear_display()
@@ -160,7 +168,13 @@ while True:
     clear_display(1)
 
     # display author
-    display_typed_text(author_label, author, cache_label=cache_author_label)
+    display_typed_text(
+        author_label,
+        authors[message_index],
+        cache_label=cache_author_label
+    )
     time.sleep(3.5)
 
     clear_display(1)
+
+    message_index = (message_index + 1) % len(messages)
