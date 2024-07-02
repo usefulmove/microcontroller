28c140403c38e861426b724d1b12342bf51eaf01
Duane Edmonds <duane.edmonds@gmail.com>
2024-07-02 08:51:53 -0700

    added instruction text

--- m5stack-atoms3/circuitpython/mobility_timer/code.py
+++ m5stack-atoms3/circuitpython/mobility_timer/code.py
@@ -16,10 +16,12 @@ screen_width = 128
 # font colors
 countdown_font_color = 0x00c0ff
 rest_font_color = 0xf9e4bc
+instruction_font_color = 0x0c0c0c
 
 # font paths
 countdown_font_path = "/fonts/Comfortaa-Regular-60.bdf"
 rest_font_path = "/fonts/Comfortaa-Regular-20.bdf"
+instruction_font_path = "/fonts/Comfortaa-Regular-16.bdf"
 
 # set up display
 splash = displayio.Group()
@@ -29,6 +31,7 @@ board.DISPLAY.root_group = splash
 # load fonts
 countdown_font = bitmap_font.load_font(countdown_font_path)
 rest_font = bitmap_font.load_font(rest_font_path)
+instruction_font = bitmap_font.load_font(instruction_font_path)
 
 # configure labels
 countdown_label = label.Label(
@@ -45,13 +48,22 @@ rest_label = label.Label(
     anchor_point=(0.5, 0.5),
     anchored_position=(screen_width//2, screen_height//2),
 )
+instruction_label = label.Label(
+    instruction_font,
+    text="",
+    color=instruction_font_color,
+    anchor_point=(0.5, 0.5),
+    anchored_position=(screen_width//2, screen_height*3//4),
+)
 
 splash.append(countdown_label)
 splash.append(rest_label)
+splash.append(instruction_label)
 
 def clear_splash(delay=0):
     countdown_label.text = ""
     rest_label.text = ""
+    instruction_label.text = ""
     time.sleep(delay)
 
 time.sleep(3)
@@ -60,6 +72,7 @@ while True:
     clear_splash(1)
 
     # rest countdown
+    instruction_label.text = "relax"
     for count in list(range(rest_time+1))[::-1]:
         rest_label.text = str(count)
         time.sleep(2)
