ed3285812b101d9c2011ce4aa5f2953a4ee30113
Duane Edmonds <duane.edmonds@gmail.com>
2024-07-05 16:47:13 -0700

    removed Lovehall quote

--- adafruit-esp32-s3-tft-feather/circuitpython/popcorn_reader/code.py
+++ adafruit-esp32-s3-tft-feather/circuitpython/popcorn_reader/code.py
@@ -36,8 +36,8 @@ author_font_path = "/fonts/Comfortaa-Regular-16.bdf"
 messages = []
 authors = []
 
-messages.append("\"Let me tell you something. This here, right now, at this very moment, is all that matters to me. I love you. And that's urgent like a motherfucker.\"")
-authors.append("Darius Lovehall")
+#messages.append("\"Let me tell you something. This here, right now, at this very moment, is all that matters to me. I love you. And that's urgent like a motherfucker.\"")
+#authors.append("Darius Lovehall")
 
 messages.append("\"It is not the critic who counts; not the man who points out how the strong man stumbles, or where the doer of deeds could have done them better. The credit belongs to the man who is actually in the arena, whose face is marred by dust and sweat and blood; who strives valiantly; who errs, who comes short again and again, because there is no effort without error and shortcoming; but who does actually strive to do the deeds; who knows great enthusiasms, the great devotions; who spends himself in a worthy cause; who at the best knows in the end the triumph of high achievement, and who at the worst, if he fails, at least fails while daring greatly, so that his place shall never be with those cold and timid souls who neither know victory nor defeat.\"")
 authors.append("Theodore\nRoosevelt Jr.")
